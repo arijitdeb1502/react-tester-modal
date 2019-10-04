56ebde2daa5f9680b6f067390b9381763d0b8506
arijitdeb1502 <arijitdeb.git@gmail.com>
2019-10-04 08:10:55 +0800

    Created the initial structure of file validator application

---
+++ src/components/FileValidatorApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class FileValidatorApp extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <h1>File Validator App</h1>
+      </div>
+    )
+  }
+}
+
